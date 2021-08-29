7440a37370e5d323b70664526473f846d5c05a83
coolbeatz71 <sigmacool@gmail.com>
2021-08-29 10:06:56 +0200

    feat: add support to ignore multiple branches separated by commas

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 #!/usr/bin/env node
 
-const branchName = require('current-git-branch')()
-const args = require('get-them-args')()
+const branchName = require("current-git-branch")();
+const args = require("get-them-args")();
 
 if (!args.unknown[0]) {
-  console.error('No regex pattern provided')
-  process.exit(1)
+  console.error("No regex pattern provided");
+  process.exit(1);
 }
 
 if (args.ignore && new RegExp(args.ignore).test(branchName)) {
-  console.warn(`Ignoring ${branchName} branch name check.`)
-  return
+  console.warn(`Ignoring ${branchName} branch name check.`);
+  return;
 }
 
-const pattern = new RegExp(args.unknown[0])
+if (args.ignore && args.ignore.indexOf(",") > -1) {
+  const argsValues = args.ignore.split(",");
+  argsValues.map(function (branch) {
+    console.warn(`Ignoring ${branch} branch name check.`);
+    return;
+  });
+}
+
+const pattern = new RegExp(args.unknown[0]);
 
 if (!pattern.test(branchName)) {
   console.error(
-    `Current branch name ${branchName} does not match the enforced naming.`
-  )
-  process.exit(1)
+    `Current branch name ${branchName} does not match the enforced naming.`,
+  );
+  process.exit(1);
 }
